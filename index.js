78e6d9fee0378209828c67f0bdf064513df6c09c
Edmound <eddnjoori@gmail.com>
2024-04-08 16:06:41 +0300

    Replaces the previous text

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded',construct)
+function construct(e){
+ const change= document.getElementById('text')
+ change.innerHTML='This is really cool!'
+}
